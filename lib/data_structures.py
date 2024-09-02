0d7ee6cb544bcc1ee9f80ebab32e0170f3f60174
MechaLarry <mecha.larry@gmail.com>
2024-09-02 23:31:31 +0300

    Working with data structures

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,49 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+  return [food["name"] for food in spicy_foods]
+print(get_names(spicy_foods))
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+      return [food for food in spicy_foods if food["heat_level"] > 5]
+print(get_spiciest_foods(spicy_foods))
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+  for food in spicy_foods:
+        heat_emoji = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_emoji}")
+print_spicy_foods(spicy_foods)
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+     for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food     
+print(get_spicy_food_by_cuisine(spicy_foods, "American"))
+print(get_spicy_food_by_cuisine(spicy_foods, "Thai"))
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+ spiciest_foods = get_spiciest_foods(spicy_foods)
+ print_spicy_foods(spiciest_foods)
+
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+     total_heat_level = sum(food["heat_level"] for food in spicy_foods)
+     return total_heat_level // len(spicy_foods)
+print(get_average_heat_level(spicy_foods))
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+ spicy_foods.append(new_food)
+ return spicy_foods
+new_food = {
+    'name': 'Griot',
+    'cuisine': 'Haitian',
+    'heat_level': 10,
+}
+
+print(create_spicy_food(spicy_foods, new_food))
